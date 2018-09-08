84f6ff7710bb98dc8b4f1085f2df69bcf5089b48
Jan Malakhovski <oxij@oxij.org>
2018-09-08 21:13:35 +0000

    Revert "nixos/tests/flatpak: explicitly disable gdm to fix eval"
    This reverts commit 7e9bd2dae1e28e8252096cc3dfdd12d353edffbd.
    
    No longer needed after the previous commit.

--- nixos/tests/installed-tests/flatpak.nix
+++ nixos/tests/installed-tests/flatpak.nix
@@ -6,8 +6,6 @@ makeInstalledTest {
 
   testConfig = {
     services.xserver.desktopManager.gnome3.enable = true; # TODO: figure out minimal environment where the tests work
-    # common/x11.nix enables the auto display manager (lightdm)
-    services.xserver.displayManager.gdm.enable = false;
     services.gnome3.core-utilities.enable = false;
     services.flatpak.enable = true;
     environment.systemPackages = with pkgs; [ gnupg ostree python2 ];
